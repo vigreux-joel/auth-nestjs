e5f9ec35fcdc9e7a7fc49ae5cc087ba2e8810863
vigreux <vigreux.joel@hotmail.com>
2022-04-25 22:28:36 +0200

    prisma user

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,12 +1,18 @@
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import {Body, Controller, Get, Post} from '@nestjs/common';
+import {UserService} from "./user/user.service";
+import { User as UserModel} from '@prisma/client';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(
+      private readonly userService: UserService,
+  ) {}
 
-  @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  @Post('user')
+  async signupUser(
+      @Body() userData: { name?: string; email: string },
+  ): Promise<UserModel> {
+    return this.userService.createUser(userData);
   }
+
 }
--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PrismaService } from './prisma/prisma.service';
+import { PrismaService } from './prisma.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService, PrismaService],
+  providers: [AppService, PrismaService, UserService],
 })
 export class AppModule {}
