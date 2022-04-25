6a86c6691507a1922f7d82d76f3f03a1f15394f4
vigreux <vigreux.joel@hotmail.com>
2022-04-25 16:38:38 +0200

    .env

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,19 @@
+
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import {Logger} from "@nestjs/common";
+import 'dotenv/config';
+
+const port = process.env.PORT;
+
 
 async function bootstrap() {
+
   const app = await NestFactory.create(AppModule);
-  await app.listen(3500);
+  await app.listen(port);
+  Logger.log(`Server started running on http://localhost:${port}`, 'Bootstrap');
 }
 bootstrap();
+
+
+
