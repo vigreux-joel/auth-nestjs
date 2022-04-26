396351690737ae439da53e94d3020db98b222450
vigreux <vigreux.joel@hotmail.com>
2022-04-26 10:32:58 +0200

    prisma user crud

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,18 +1,10 @@
 import {Body, Controller, Get, Post} from '@nestjs/common';
 import {UserService} from "./user/user.service";
-import { User as UserModel} from '@prisma/client';
 
 @Controller()
 export class AppController {
   constructor(
       private readonly userService: UserService,
   ) {}
-
-  @Post('user')
-  async signupUser(
-      @Body() userData: { name?: string; email: string },
-  ): Promise<UserModel> {
-    return this.userService.createUser(userData);
-  }
 
 }
