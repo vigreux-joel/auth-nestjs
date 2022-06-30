5c73096d8a8731fda6749d6ee442bea99102a5e6
vigreux-joel <vigreux.joel@hotmail.com>
2022-06-30 14:57:44 +0200

    first commit

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   await app.listen(3000);
 
-  console.log('à vchfvdvfdangé')
+  console.log('test')
 }
 bootstrap();
